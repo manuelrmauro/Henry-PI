2b00ed741aefedca0e4486a5d7cc54d338745e2f
Manuel <manuelrmauro@gmail.com>
2021-11-20 18:38:03 -0300

    router get

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -9,7 +9,7 @@ router.get('/', async function (req, res) {
 	
 	try {
 		//DATABASE
-		const dbRecipes = await Recipe.findAll({
+		let dbRecipes = await Recipe.findAll({
 			include: [
 				{
 					model: Step,
@@ -23,12 +23,19 @@ router.get('/', async function (req, res) {
 				[Diet,'id','ASC'],
 				['id', 'ASC']
 			]
-		});
-		return res.json(dbRecipes)
+		})
+		// adapta la lista para que encaje con la informacion traida de la api
+		dbRecipes = dbRecipes.map(el => el.get({ plain: true }))
+		dbRecipes.forEach(recipe => {
+			recipe.diets = recipe.diets.map(diet => diet.name)
+			recipe.analyzedInstructions = [{steps : recipe.steps.map(step => ({number :step.number, step : step.step}))}] 
+			delete recipe.steps
+		})
+
 		// EXTERNAL API
 		const eaRecipes = await axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=de640b59de324223afa040ca7fe90dad&addRecipeInformation=true&number=100')
 		.then(res => res.data.results)
-
+		console.log(eaRecipes.length)
 		res.json([...dbRecipes,...eaRecipes]);
 	} catch (error) {
 		console.log(error)
