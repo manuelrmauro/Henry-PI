dca4d5e772c5ceff49727ae826ea6cafd2f0f75d
Manuel <manuelrmauro@gmail.com>
2021-11-20 23:32:05 -0300

    init front

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav'
+import Home from './components/Nav/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Henry Food</h1>
+      <Route path='/' component={Nav}/>
+      <Route exact path='/home' component={Home}/>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+
+};
+
+function rootReducer(state = initialState, { type, payload }) {
+	switch (type) {
+
+		default:
+			return state;
+	}
+}
+
+export default rootReducer;
