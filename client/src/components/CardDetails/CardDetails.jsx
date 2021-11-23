13315658dfa97617e16fad9c8b3d9f58ea94c447
Manuel <manuelrmauro@gmail.com>
2021-11-23 12:55:18 -0300

    card details

--- client/src/components/CardDetails/CardDetails.jsx
+++ client/src/components/CardDetails/CardDetails.jsx
@@ -8,14 +8,30 @@ function CardDetails({ match, recipe, getRecipeDetails }) {
 	useEffect(() => {
 		getRecipeDetails(id);
 	}, []);
+	/* 
+	[ ] Los campos mostrados en la ruta principal para cada receta (imagen, nombre, tipo de plato y tipo de dieta)
+	[ ] Resumen del plato
+	[ ] Puntuación
+	[ ] Nivel de "comida saludable"
+	[ ] Paso a paso */
 
 	return (
 		<div>
 			{recipe ? (
 				Object.keys(recipe).length ? (
 					<div>
-						{recipe.id}
-						{recipe.title}
+						<h1>{recipe.title}</h1>
+						<img src={recipe.image} width="100" alt="food" />
+						<p>{recipe.summary}</p>
+						<p>Score : {recipe.spoonacularScore}</p>
+						<p>Health Score: {recipe.healthScore}</p>
+						{recipe.diets.map((diet) => (
+							<p>{diet}</p>
+						))}
+						{recipe.analyzedInstructions[0].steps.map(step => <div>
+							<h2>PASO {step.number}</h2>
+							<p>{step.step}</p>
+						</div>)}
 					</div>
 				) : (
 					'ERROR 404 not found'
